fa04fa9330daa84215ce6f96787a3d1f04c5d221
Noitidart <noitidart@gmail.com>
2014-09-11 19:17:45 -0700

    Update bootstrap.js
    importing subprocess from ipcode works. i added an exmaple here

--- bootstrap.js
+++ bootstrap.js
@@ -7,7 +7,6 @@ const self = {
 };
 Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/devtools/Console.jsm');
-Cu.import('resource://gre/modules/FileUtils.jsm');
 
 
 function install() {}
@@ -18,6 +17,26 @@ function startup() {
 	//import self modules
 	Cu.import(self.path + 'modules/subprocess.jsm');
 	console.info(subprocess);
+	
+	if (Services.appinfo.OS == 'WINNT') {
+		var ps = "C:\\windows\\system32\\tasklist.exe";
+		var args = [];
+		
+		var buffer = [];
+		subprocess.call({
+			command: ps,
+			arguments: args,
+			stdout: function(data) {
+				buffer.push(data);
+			},
+			done: function() {
+				//var lines = buffer.join('').split('\n');
+				console.log('buffer:', buffer)
+			}
+		});
+	} else {
+		//subprocess example for non windows not yet implemented but plan to jack from here: https://github.com/ochameau/adbhelper/blob/fd3073a0f5f51e1707a5b4cbace82b13fb5a1fe5/adb.js#L192
+	}
 }
  
 function shutdown(aReason) {
