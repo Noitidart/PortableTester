dbc9627b58162ffcb96e104ded1e1c06f25dd9ea
Noitidart <noitidart@gmail.com>
2014-11-08 10:58:37 -0800

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,12 +1,12 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import('resource://gre/modules/Services.jsm');
-//var stringBundle = Services.strings.createBundle("chrome://portabletester/locale/global.properties?" + Math.random()); // Randomize URI to work around bug 719376
+var stringBundle = Services.strings.createBundle("chrome://portabletester/locale/global.properties?" + Math.random()); // Randomize URI to work around bug 719376
 
 function install() {}
 function uninstall() {}
 function startup() {
  Services.prompt.alert(null, 'here', 'heeere');
- //Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.GetStringFromName('startup_prompt_message'))
+ Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.GetStringFromName('startup_prompt_message'))
 }
  
 function shutdown() {}
